6d512ae82ccd29ce221d56041a96e3d800a7c287
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-01-05 19:21:50 -0300

    first commit

---
+++ src/main/java/com/devsuperior/challengethree/ChallengethreeApplication.java
@@ -0,0 +1,14 @@
+package com.devsuperior.challengethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengethreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengethreeApplication.class, args);
+		
+	}
+
+}
